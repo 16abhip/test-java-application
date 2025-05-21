bf9db280d84ca68bf8ea793f56e527e9833dcc1d
Abhishek Singh <abhisheksingh@Abhisheks-MacBook-Air-2.local>
2025-05-21 22:17:04 +0530

    added ok checked

--- src/main/java/com/codingshuttle/anuj/prod_ready_features/prod_ready_features/controllers/HealthCheckController.java
+++ src/main/java/com/codingshuttle/anuj/prod_ready_features/prod_ready_features/controllers/HealthCheckController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
     @GetMapping("/")
     public ResponseEntity<String> healthCheckController(){
-        return ResponseEntity.ok("ok");
+        return ResponseEntity.ok("OK, CHECKED");
     }
 }
